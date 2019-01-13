c7241fc31dd397b69457b797de48447d1b9de376
Sean Lin <seanlin1016@gmail.com>
2019-01-13 22:30:17 +0800

    start drawing lines with context

---
+++ CanvasDrawing/ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  CanvasDrawing
+//
+//  Created by SEAN on 2019/1/13.
+//  Copyright © 2019 SEAN. All rights reserved.
+//
+
+import UIKit
+
+class Canvas: UIView {
+    var lines = [[CGPoint]]()
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        
+        guard let context = UIGraphicsGetCurrentContext() else { return }
+        context.setLineCap(.round)
+        context.setStrokeColor(UIColor.green.cgColor)
+        context.setLineWidth(8)
+        
+        lines.forEach { (line) in
+            for (index, p) in line.enumerated() {
+                if index == 0 {
+                    context.move(to: p)
+                } else {
+                    context.addLine(to: p)
+                }
+            }
+        }
+        
+        context.strokePath()
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        lines.append([CGPoint]())
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let point = touches.first?.location(in: nil) else { return }
+        
+        guard var lastLine = lines.popLast() else { return }
+        lastLine.append(point)
+        lines.append(lastLine)
+        setNeedsDisplay()
+    }
+}
+
+class ViewController: UIViewController {
+    
+    let canvas = Canvas()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(canvas)
+        canvas.backgroundColor = .white
+        canvas.frame = view.frame
+    }
+
+
+}
+
